202f6b93c6d82d2baf0dd5237b6a24b49c2f01c6
Lucas <48624354+Xonax@users.noreply.github.com>
2025-02-12 09:58:55 +0100

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect('admin');
+});
+
+Route::get('/dashboard', function () {
+    return redirect('/admin');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('cars', CarController::class);
+});
+
+Route::get('/admin/invoices/{file}', [InvoiceController::class, 'download'])
+    ->middleware(['auth']) // Ensure the user is authenticated
+    ->name('admin.invoices.download');
